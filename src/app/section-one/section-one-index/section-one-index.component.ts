e4267beaa4baa7a2f885e30f92cde4a92aa27a1c
Since199x <chaturapak.bo.58@ubu.ac.th>
2020-04-12 18:20:34 +0700

    modified section-one

--- src/app/section-one/section-one-index/section-one-index.component.ts
+++ src/app/section-one/section-one-index/section-one-index.component.ts
@@ -10,63 +10,17 @@ export class SectionOneIndexComponent implements OnInit {
   socket: any;
   users: [] = [];
   input : any;
-  constructor() { 
+  constructor() {
     this.socket = io.connect('ws://localhost:3250');
   }
 
   ngOnInit() {
-    this.initUser();
-  }
-
-  onChangeInput(){
-    this.socket.emit('news', 'hello my news');
-    this.socket.on('news', (data) => {
-      console.log('my data', data)
+    this.socket.on('some', (data) => {
+      console.log('response from server', data);
     });
-
-    // this.socket.emit('my other event', function (data) {
-    //   console.log(data);
-    //   this.socket.on('news', { my: 'data' });
-    // });
-
-    // console.log('input val : ', this.input);
-    // let shareDate = JSON.stringify(this.users);
-    // this.socket.emit('news', shareDate);
-    // this.socket.on('news', (data) => {
-    //   console.log(' ok ' ,data);
-    //   this.socket.emit('news' , {my : 'lk'})
-    // })
-    // let observable = new Observable(observer => {
-    //   this.socket.on('news', (data) => {
-    //     console.log("Received message from Websocket Server")
-    //     this.socket.emit('news', {my : 'data'});
-    //     observer.next(data);
-    //     })
-    //     return () => {
-    //       console.log('fail');
-    //       this.socket.disconnect();
-    //     }
-    // });
-    // observable.subscribe(res=> {
-    //   console.log('result ' , res);
-    // })
   }
 
-  initUser(){
-    // let userOne = {
-    //   username : 'timmy',
-    //   userId : 0,
-    //   fullname : 'timmy',
-    //   displayname : 'timmy'
-    // };
-    // this.users.push(userOne);
-
-    // let userTwo = {
-    //   username : 'timmy2',
-    //   userId : 1,
-    //   fullname : 'timmy2',
-    //   displayname : 'timmy2'
-    // };
-    // this.users.push(userTwo);
+  onClickInput(){
+    this.socket.emit('CREATE', 'create');
   }
 }
